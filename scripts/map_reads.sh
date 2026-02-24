265028f0861598a296cbbaf623f351fff86c17f8
Adegite Adejuwon <88291164+a-adegite@users.noreply.github.com>
2026-02-24 17:52:51 +0100

    updated genome alignment script

--- scripts/map_reads.sh
+++ scripts/map_reads.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Path to the directory containing input FASTQ files (trimmed reads)
 input_path="../data/repaired_reads/"
 
@@ -15,24 +17,30 @@ ref_genome="${mapping_dir}mycobacterium_tuberculosis.fna"
 # Make sure output directory exists
 mkdir -p "${output_path}"
 
-# Index the reference genome with bwa-mem2 
-echo "Indexing reference genome: ${ref_genome}"
-bwa-mem2 index "${ref_genome}"
+# Index the reference genome with bwa-mem2
+bwa-mem2 index "${mapping_dir}"/mycobacterium_tuberculosis.fna
 
 for file in "${input_path}"*_1.fastq.gz
 do
-    # Extract the sample name by removing the directory path and the suffix _1.fastq.gz
-    sample=$(basename "$file" _1.fastq.gz)
+    # Extract the base name (without _1.fastq.gz)
+    sample=$(basename "${file}" "_1.fastq.gz")
     echo "Mapping sample: ${sample}"
 
-    # Run bwa-mem2 mem to map paired reads to the reference genome and save sam file
-    bwa-mem2 mem "${ref_genome}" \
-        "${input_path}/${sample}_1.fastq.gz" "${input_path}/${sample}_2.fastq.gz" > "${output_path}${sample}.sam"
+     # Run bwa-mem2 mem to map paired reads to the reference genome and save sam file
+     bwa-mem2 mem \
+        -t 8 \
+        "${ref_genome}" \
+        "${input_path}/${sample}_1.fastq.gz" \
+        "${input_path}/${sample}_2.fastq.gz" > \
+        "${output_path}${sample}.sam" 
 
     # Convert sam file to bam file
-    samtools view -Sb "${output_path}${sample}.sam" > "${output_path}${sample}.bam"
+    samtools view -b -o "${output_path}${sample}.bam" "${output_path}${sample}.sam" 
 
-    echo "Completed mapping for ${sample}"
+     echo "Completed mapping for ${sample}"
 done
 
 echo "All samples processed"
+
+     
+
